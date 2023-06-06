ca87468023edc334e2dbc3739c67bb3f5245d9a1
nfottz <yblee122077@gmail.com>
2023-06-06 19:57:31 +0900

    second commit_yb

--- src/main/java/com/gdu/workship/domain/ApprovalReferenceDTO.java
+++ src/main/java/com/gdu/workship/domain/ApprovalReferenceDTO.java
@@ -1,13 +1,8 @@
 package com.gdu.workship.domain;
 
-import java.util.Date;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import com.gdu.workship.domain.ApprovalDTO;
-import com.gdu.workship.domain.MemberDTO;
 
 @Data
 @NoArgsConstructor
