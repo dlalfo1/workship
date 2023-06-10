8dbce8c847a354de032b41f978f9ebe32e685743
kjh626 <wjd1113@naver.com>
2023-06-10 17:59:03 +0900

    kimjh

--- src/main/java/com/gdu/workship/controller/NoticeBoardController.java
+++ src/main/java/com/gdu/workship/controller/NoticeBoardController.java
@@ -1,11 +1,34 @@
 package com.gdu.workship.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.gdu.workship.service.NoticeBoardService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class NoticeBoardController {
 
+  private final NoticeBoardService noticeBoardService;
+  
+  @GetMapping("/notice/noticeList.do")
+  public String noticeMain(HttpServletRequest request, Model model) {
+    noticeBoardService.loadNoticeBoardList(request, model);
+    return "notice/noticeMain";
+  }
+  
+  @GetMapping("/notice/noticeDetail.html")
+  public String noticeDetail(@RequestParam(value="noticeNo", required=false, defaultValue="0") int noticeNo, Model model) {
+    noticeBoardService.getNoticeByNo(noticeNo, model);
+    return "notice/noticeDetail";
+  }
+  
 	@GetMapping("/notice/write.html")
 	public String write() {
 		return "notice/noticeWrite";
