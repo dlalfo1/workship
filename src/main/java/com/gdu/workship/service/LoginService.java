4548d877951644257c43e08052d5a874d3cf0581
njh7554 <njh7554@naver,com>
2023-06-13 17:15:29 +0900

    jh

--- src/main/java/com/gdu/workship/service/LoginService.java
+++ src/main/java/com/gdu/workship/service/LoginService.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 public interface LoginService {
 	
 	public void login(HttpServletRequest request, HttpServletResponse response);
-	// public void autologin(HttpServletRequest request, HttpServletResponse response);
+	public void autologin(HttpServletRequest request, HttpServletResponse response);
 	public void logout(HttpServletRequest request, HttpServletResponse response);
 	
 
