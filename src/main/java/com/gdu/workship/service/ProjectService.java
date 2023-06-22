680a3f14e44d31535c9b66525015955a5eb110d9
komewel <kimcomewel@gmail.com>
2023-06-22 15:13:54 +0900

    kyh

--- src/main/java/com/gdu/workship/service/ProjectService.java
+++ src/main/java/com/gdu/workship/service/ProjectService.java
@@ -8,9 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 public interface ProjectService {
-	public int addProjectM(MultipartHttpServletRequest multipartHttpRequest);
+	public int addProjectM(HttpServletRequest request);
 	public int deleteProjectM(int projectNo);
-	public int updateProjectM(MultipartHttpServletRequest multipartHttpRequest);
+	public int updateProjectM(HttpServletRequest request);
 	public int addProjectW(MultipartHttpServletRequest multipartHttpRequest);
 	public int deleteProjectW(int projectWorkNo);
 	public int updateProjectW(MultipartHttpServletRequest multipartrequest);
