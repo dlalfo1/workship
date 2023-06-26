f3f24f7949b4d31b1fa4957eb55d425a05f6a86e
pakgeunhee <pakgeunhee@gmail.com>
2023-06-26 11:04:11 +0900

    hee

--- src/main/java/com/gdu/workship/service/MailServiceImpl.java
+++ src/main/java/com/gdu/workship/service/MailServiceImpl.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
